e2d7483147572d6bac385583d253ff33541dba46
Wayne Wang <wwang@liventus.com>
2016-12-13 16:47:54 -0600

    Initial commit

---
+++ src/main/lc_medium/Kth_smallest_element_in_sorted_matrix.java
@@ -0,0 +1,14 @@
+package main.lc_medium;
+
+import java.util.LinkedList;
+
+/**
+ * Created by wwang on 12/7/2016.
+ */
+public class Kth_smallest_element_in_sorted_matrix {
+
+    public static void main(String[] args) {
+        LinkedList<String> s = new LinkedList<>();
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/lc_medium/Kth_smallest_element_in_sorted_matrixTest.java
@@ -0,0 +1,19 @@
+package test.lc_medium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by wwang on 12/7/2016.
+ */
+class Kth_smallest_element_in_sorted_matrixTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+    }
+
+}
