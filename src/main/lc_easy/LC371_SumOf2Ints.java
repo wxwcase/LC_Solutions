2f03c2469f0aa85c6ecf646073a474de0cdfb2c3
Wayne Wang <wwang@liventus.com>
2016-12-15 10:49:40 -0600

    Add LC 371 code and test

---
+++ src/main/lc_easy/LC371_SumOf2Ints.java
@@ -0,0 +1,23 @@
+package main.lc_easy;
+
+/**
+ * Created by wwang on 12/15/2016.
+ */
+public class LC371_SumOf2Ints {
+
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int c = (a & b) << 1;
+            a ^= b;
+            b = c;
+        }
+        return a;
+//        int c = a & b;
+//        while (b != 0) {
+//            a = a ^ b;
+//            b = c << 1;
+//            c = a & b;
+//        }
+//        return a;
+    }
+}
---
+++ src/test/lc_easy/LC371_SumOf2IntsTest.java
@@ -0,0 +1,39 @@
+package test.lc_easy;
+
+import main.lc_easy.LC371_SumOf2Ints;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by wwang on 12/15/2016.
+ */
+class LC371_SumOf2IntsTest {
+
+    LC371_SumOf2Ints sol;
+
+    @BeforeEach
+    void setUp() {
+        sol = new LC371_SumOf2Ints();
+    }
+
+    @AfterEach
+    void tearDown() {
+        sol = null;
+    }
+
+    @Test
+    void getSum1and2() {
+        int a = 1, b = 2;
+        assertEquals(3, sol.getSum(a, b));
+    }
+
+    @Test
+    void getSum10and21() {
+        int a = 10, b = 21;
+        assertEquals(31, sol.getSum(a, b));
+    }
+
+}
