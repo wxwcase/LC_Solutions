a384411858151d309c33c80e97dcf7ee3434f886
Wayne Wang <wwang@liventus.com>
2016-12-20 17:17:45 -0600

    LC350: added sorted input solution.

--- src/main/lc_easy/LC350_IntersectionOfTwoArraysII.java
+++ src/main/lc_easy/LC350_IntersectionOfTwoArraysII.java
@@ -7,6 +7,47 @@ import java.util.*;
  */
 public class LC350_IntersectionOfTwoArraysII {
 
+    public int[] intersectPresorted(int[] nums1, int[] nums2) {
+
+        if (nums1 == null
+                || nums1.length == 0
+                || nums2 == null
+                || nums2.length == 0)
+            return new int[]{};
+
+        int l1 = nums1.length, l2 = nums2.length;
+        int p1 = 0, p2 = 0;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> list = new LinkedList<>();
+
+        while (p1 < l1 && p2 < l2) {
+
+           if (nums1[p1] == nums2[p2]) {
+                list.add(nums1[p1]);
+                ++p1;
+                ++p2;
+           }
+
+           while (p1 < l1 && p2 < l2 && nums1[p1] < nums2[p2]) {
+               ++p1;
+           }
+
+           while (p1 < l1 && p2 < l2 && nums1[p1] > nums2[p2]) {
+               ++p2;
+           }
+        }
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
+
     public int[] intersect(int[] nums1, int[] nums2) {
 
         Map<Integer, Integer> map = new HashMap<>();
--- src/test/lc_easy/LC350_IntersectionOfTwoArraysIITest.java
+++ src/test/lc_easy/LC350_IntersectionOfTwoArraysIITest.java
@@ -30,35 +30,45 @@ class LC350_IntersectionOfTwoArraysIITest {
     void intersect01() {
         int[] n1 = {1, 2, 2, 1}, n2 = {2, 2}, r1 = {2, 2};
         int[] res = sol.intersect(n1, n2);
+        int[] res2 = sol.intersectPresorted(n1, n2);
         Arrays.sort(r1);
         Arrays.sort(res);
         assertArrayEquals(r1, res);
+        assertArrayEquals(r1, res2);
     }
 
     @Test
     void intersect02() {
         int[] n1 = {1, 2}, n2 = {1, 1}, r1 = {1};
-        int [] res = sol.intersect(n1, n2);
+        int[] res = sol.intersect(n1, n2);
+        int[] res2 = sol.intersectPresorted(n1, n2);
         Arrays.sort(res);
         Arrays.sort(r1);
         assertArrayEquals(r1, res);
+        assertArrayEquals(r1, res2);
     }
 
     @Test
     void intersect03() {
         int[] n1 = {9, 1}, n2 = {7, 8, 3, 9, 0, 0, 9, 1, 5}, r1 = {1, 9};
         int[] res = sol.intersect(n1, n2);
+        int[] res2 = sol.intersectPresorted(n1, n2);
         Arrays.sort(r1);
         Arrays.sort(res);
+        Arrays.sort(res2);
         assertArrayEquals(r1, res);
+        assertArrayEquals(r1, res2);
     }
 
     @Test
     void intersect04() {
         int[] n1 = {7, 2, 2, 4, 7, 0, 3, 4, 5}, n2 = {3, 9, 8, 6, 1, 9}, r1 = {3};
         int[] res = sol.intersect(n1, n2);
+        int[] res2 = sol.intersectPresorted(n1, n2);
         Arrays.sort(res);
+        Arrays.sort(res2);
         Arrays.sort(r1);
         assertArrayEquals(r1, res);
+        assertArrayEquals(r1, res2);
     }
 }
