e7d37f145b9c7891b99dfe87d8a7874a9dd75b72
Wayne Wang <wwang@liventus.com>
2016-12-23 17:28:57 -0600

    Added LC 459, not completed.

---
+++ src/main/lc_easy/LC459_RepeatedSubstringPattern.java
@@ -0,0 +1,17 @@
+package main.lc_easy;
+
+/**
+ * Created by wwang on 12/23/2016.
+ */
+public class LC459_RepeatedSubstringPattern {
+
+    public boolean repeatedSubstringPattern(String str) {
+
+        if (str == null || str.length() == 0)
+            return false;
+
+
+
+        return true;
+    }
+}
