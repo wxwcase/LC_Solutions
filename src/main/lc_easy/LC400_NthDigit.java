47d7eb2bc593ed81f9549d9f568ec772e62cf6e1
wxwcase <weicwru@gmail.com>
2017-01-11 22:58:55 -0600

    Added LC 400, not started yet.

---
+++ src/main/lc_easy/LC400_NthDigit.java
@@ -0,0 +1,14 @@
+package main.lc_easy;
+
+/**
+ * Created by wxwcase on 1/11/17.
+ */
+public class LC400_NthDigit {
+
+    public int findNthDigit(int n) {
+
+        if (n < 10) return n;
+
+        return 0;
+    }
+}
