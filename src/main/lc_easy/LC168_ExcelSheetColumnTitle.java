d63662c17bff63eaf6d80410dc9dea74cd95d50e
wxwcase <weicwru@gmail.com>
2017-01-16 22:08:23 -0600

    Added LC 168.

--- src/main/lc_easy/LC168_ExcelSheetColumnTitle.java
+++ src/main/lc_easy/LC168_ExcelSheetColumnTitle.java
@@ -5,8 +5,27 @@ package main.lc_easy;
  */
 public class LC168_ExcelSheetColumnTitle {
 
+    // Non-recursive solution
     public String convertToTitle(int n) {
 
-        return "";
+        if (n < 0) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(n-- > 26) {
+            sb.append((char)(n % 26 + 'A'));
+            n /= 26;
+        }
+
+        sb.append((char)(n % 26 + 'A'));
+
+        return sb.reverse().toString();
+    }
+
+    // Recursive solution
+    public String convertToTitleRecursive(int n) {
+        return n == 0 ? "" : convertToTitleRecursive(--n / 26) + (char)('A' + n % 26);
     }
 }
