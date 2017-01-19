5208db4a83394ba03a4d81f0e819fef195294640
Wayne Wang <wwang@liventus.com>
2017-01-19 12:39:33 -0600

    Added LC 260.

---
+++ src/main/lc_medium/LC260_SingleNumberIII.java
@@ -0,0 +1,44 @@
+package main.lc_medium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by wwang on 1/19/2017.
+ */
+public class LC260_SingleNumberIII {
+
+    /**
+     * Idea:
+     * XOR all elements to diff, find the last digit that is 1. This means the digit
+     * is different between those two numbers. Imagine put all the numbers into two
+     * groups, the first with the specified digit set to 0, the other set to 1. Then
+     * XOR all elements that set the digit to 0, XOR all elements that set to 1.
+     * @param nums
+     * @return
+     */
+    public int[] singleNumber(int[] nums) {
+
+        if (nums == null || nums.length < 2) return nums;
+
+        int diff = 0;
+        for (int i : nums) {
+            diff ^= i;
+        }
+
+        // find the last digit the two numbers differs
+        diff &= -diff;
+
+        int[] res = {0, 0};
+
+        for (int n : nums) {
+            if ((n & diff) == 0) {
+                res[0] ^= n;
+            } else {
+                res[1] ^= n;
+            }
+        }
+
+        return res;
+    }
+}
