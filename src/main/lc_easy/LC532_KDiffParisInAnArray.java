22d95c670f400cb3f14d96a2d3046c9ec5fffb9c
Wayne Wang <wwang@liventus.com>
2017-03-06 17:24:49 -0600

    Added LC 69.

---
+++ src/main/lc_easy/LC532_KDiffParisInAnArray.java
@@ -0,0 +1,14 @@
+package main.lc_easy;
+
+/**
+ * Created by wwang on 3/6/2017.
+ */
+public class LC532_KDiffParisInAnArray {
+
+    public int findPairs(int[] nums, int k) {
+
+        // sort and maintain distinct element
+
+        // iterate and find item k steps away
+    }
+}
---
+++ src/test/lc_easy/LC532_KDiffParisInAnArrayTest.java
@@ -0,0 +1,48 @@
+package test.lc_easy;
+
+import main.lc_easy.LC532_KDiffParisInAnArray;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by wwang on 3/6/2017.
+ */
+class LC532_KDiffParisInAnArrayTest {
+
+    LC532_KDiffParisInAnArray sol;
+
+    @BeforeEach
+    void setUp() {
+        sol = new LC532_KDiffParisInAnArray();
+    }
+
+    @AfterEach
+    void tearDown() {
+        sol = null;
+    }
+
+    @Test
+    void findPairsK2() {
+        int[] n = {3, 1, 4, 1, 5};
+        int k = 2;
+        assertEquals(2, sol.findPairs(n, k));
+    }
+
+    @Test
+    void findParisK1() {
+        int[] n = {1, 2, 3, 4, 5};
+        int k = 1;
+        assertEquals(4, sol.findPairs(n, k));
+    }
+
+    @Test
+    void findParisK0() {
+        int[] n = {1, 3, 1, 5, 4};
+        int k = 0;
+        assertEquals(1, sol.findPairs(n, k));
+    }
+
+}
