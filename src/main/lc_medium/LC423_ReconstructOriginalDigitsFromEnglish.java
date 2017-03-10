f22e18c64c8a6a03dda726f5e5127eac0c81e663
Wayne Wang <wwang@liventus.com>
2017-03-10 16:50:11 -0600

    Added LC 423, not completed.

---
+++ src/main/lc_medium/LC423_ReconstructOriginalDigitsFromEnglish.java
@@ -0,0 +1,11 @@
+package main.lc_medium;
+
+/**
+ * Created by WWang on 3/10/2017.
+ */
+public class LC423_ReconstructOriginalDigitsFromEnglish {
+
+    public String originalDigits(String s) {
+
+    }
+}
