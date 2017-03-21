3286c76615c024ef3574930d6ed08164791cc107
Wayne Wang <wwang@liventus.com>
2017-03-21 15:24:45 -0500

    Added a solution to LC 145.

--- src/main/lc_hard/LC145_BinaryTreePostorderTraversal.java
+++ src/main/lc_hard/LC145_BinaryTreePostorderTraversal.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 public class LC145_BinaryTreePostorderTraversal {
 
+    // two stack
     public List<Integer> postorderTraversal(TreeNode root) {
 
         List<Integer> res = new ArrayList<>();
@@ -32,6 +33,27 @@ public class LC145_BinaryTreePostorderTraversal {
 
         while (!s2.isEmpty()) {
             res.add(s2.pop().val);
+        }
+
+        return res;
+    }
+
+    // one stack
+    public List<Integer> solution2(TreeNode root) {
+
+        List<Integer> res = new LinkedList<>();
+
+        if (root == null) return res;
+
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode n = stack.pop();
+            res.add(0, n.val);
+            if (n.left != null) stack.push(n.left);
+            if (n.right != null) stack.push(n.right);
         }
 
         return res;
